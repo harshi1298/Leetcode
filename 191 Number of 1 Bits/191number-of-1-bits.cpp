81bf739b971b067b06e145b1d8c9929d5ee8ba09
harshi1298 <11082004harshit@gmail.com>
2024-01-18 11:51:39 +0000

    Code challenge submission for question: 191 Number of 1 Bits

---
+++ 191 Number of 1 Bits/191number-of-1-bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int l=0;
+        for(int i=0;i<32;i++){
+            if(n%2!=0)
+             l++;
+            n= n>>1;
+             cout<<n<<endl;            
+        }
+        return l;
+    }
+};
