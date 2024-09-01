cb8e954592c9f72529b25a9c6aa2af573770c586
harshi1298 <11082004harshit@gmail.com>
2024-09-01 12:57:45 +0000

    Code challenge submission for question: 235 Lowest Common Ancestor of a Binary Search Tree

---
+++ 235 Lowest Common Ancestor of a Binary Search Tree/235lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+private:
+   TreeNode* solve(TreeNode* root,TreeNode* p, TreeNode* q){
+     if(root==NULL){
+        return root;
+     }
+     if(root->val>p->val && root->val>q->val){
+        return solve(root->left,p,q);
+     }
+     else if(root->val<p->val && root->val<q->val){
+        return solve(root->right,p,q);
+     }
+        return root;
+   }
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        return solve(root,p,q);
+    }
+};
