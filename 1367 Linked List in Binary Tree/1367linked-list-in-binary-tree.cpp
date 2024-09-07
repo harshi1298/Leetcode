33f368818e00e88521ea7bd18a30618515f6dff0
harshi1298 <11082004harshit@gmail.com>
2024-09-07 04:11:13 +0000

    Code challenge submission for question: 1367 Linked List in Binary Tree

---
+++ 1367 Linked List in Binary Tree/1367linked-list-in-binary-tree.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    bool solve(ListNode* head,TreeNode* root){
+        if(root==NULL || head==NULL){
+            if(head==NULL)return true;
+            else return false;
+        }
+        if(head->val==root->val){
+            return solve(head->next,root->left) || solve(head->next,root->right);
+        }
+        else return false;
+    }
+public:
+    bool isSubPath(ListNode* head, TreeNode* root) {
+      if(root==NULL)return head==NULL;
+      if(solve(head,root))return true;
+      else return isSubPath(head,root->left) || isSubPath(head,root->right);
+    }
+};
