73c78fd38d59583e2414455f9a472e8282b30055
harshi1298 <11082004harshit@gmail.com>
2024-11-23 08:52:36 +0000

    Code challenge submission for question: 210 Course Schedule II

---
+++ 210 Course Schedule II/210course-schedule-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> findOrder(int size, vector<vector<int>>& pre) {
+          vector<vector<int>>adj(size);
+        for(int i=0;i<pre.size();i++){
+            adj[pre[i][1]].push_back(pre[i][0]);
+        }
+       vector<int>indegree(size,0);
+      for(int i=0;i<size;i++){
+         for(auto j:adj[i]){
+             indegree[j]++;
+         }
+      }
+    vector<int>ans;
+    queue<int>que;
+    for(int i=0;i<size;i++){
+        if(indegree[i]==0)que.push(i);
+    }
+    while(!que.empty()){
+       int k=que.front();
+       ans.push_back(k);
+       que.pop();
+       for(auto j :adj[k]){
+           indegree[j]--;
+           if(indegree[j]==0)que.push(j);
+       }
+    }
+    return ans.size()==size?ans:vector<int>();
+    }
+};
