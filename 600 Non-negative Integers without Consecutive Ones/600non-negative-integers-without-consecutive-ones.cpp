18d20e25a59b8a03dfe138e4d37e14f10bf5eb23
harshi1298 <11082004harshit@gmail.com>
2025-04-11 12:21:14 +0000

    Code challenge submission for question: 600 Non-negative Integers without Consecutive Ones

---
+++ 600 Non-negative Integers without Consecutive Ones/600non-negative-integers-without-consecutive-ones.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findIntegers(int n) {
+        vector<int> dp(32, 0);
+        dp[0] = 1;
+        dp[1] = 2;
+        for (int i = 2; i < 32; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2]; 
+        }
+
+        int ans = 0, prevBit = 0;
+        for (int i = 30; i >= 0; i--) {
+            if ((n & (1 << i)) != 0) {
+                ans += dp[i];
+                if (prevBit == 1) {
+                    return ans;
+                }
+                prevBit = 1;
+            } else {
+                prevBit = 0;
+            }
+        }
+        return ans + 1;
+    }
+};
