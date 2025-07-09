85141c81901673140a28bae9c59a44caf26f5003
harshi1298 <11082004harshit@gmail.com>
2025-07-09 08:06:35 +0000

    Code challenge submission for question: 221 Maximal Square

---
+++ 221 Maximal Square/221maximal-square.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int maximalSquare(vector<vector<char>> &matrix)
+    {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int maxsquare = 0;
+
+        vector<vector<int>> dp(row, vector<int>(col, 0));
+
+        for (int i = 0; i < row; ++i)
+        {
+            for (int j = 0; j < col; ++j)
+            {
+                if (matrix[i][j] == '1')
+                {
+                    if (i == 0 || j == 0)
+                    {
+                        dp[i][j] = 1;  
+                    }
+                    else
+                    {
+                        dp[i][j] = min({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]}) + 1;
+                    }
+                    maxsquare = max(maxsquare, dp[i][j]);
+                }
+            }
+        }
+
+        return maxsquare * maxsquare;
+    }
+};
