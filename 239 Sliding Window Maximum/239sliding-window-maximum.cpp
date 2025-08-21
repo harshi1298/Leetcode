0d82a9332dd546a990498b8114e2edba14ae4304
harshi1298 <11082004harshit@gmail.com>
2025-08-21 08:41:24 +0000

    Code challenge submission for question: 239 Sliding Window Maximum

---
+++ 239 Sliding Window Maximum/239sliding-window-maximum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        priority_queue<pair<int,int>>pq;
+        int n=nums.size();
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            pq.push(make_pair(nums[i],i));
+            while(!pq.empty() && pq.top().second<=i-k)pq.pop();
+            if(i>=k-1)
+            ans.push_back(pq.top().first);
+        }
+        return ans;
+    }
+};
