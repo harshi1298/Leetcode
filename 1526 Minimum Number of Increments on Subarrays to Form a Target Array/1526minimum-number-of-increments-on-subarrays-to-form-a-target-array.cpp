05f116ee970eb6e9ae21286c24e47468f2ef1530
harshi1298 <11082004harshit@gmail.com>
2025-10-30 07:37:43 +0000

    Code challenge submission for question: 1526 Minimum Number of Increments on Subarrays to Form a Target Array

---
+++ 1526 Minimum Number of Increments on Subarrays to Form a Target Array/1526minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int minNumberOperations(vector<int>& target) {
+        int ans=target[0];
+        for(int i=1;i<target.size();i++)ans+=max(0,target[i]-target[i-1]);
+        return ans;
+    }
+};
