49132ea3d6d4d2c2f919286a1b68e7f6823bee55
harshi1298 <11082004harshit@gmail.com>
2025-12-21 06:53:05 +0000

    Code challenge submission for question: 3781 Maximum Score After Binary Swaps

---
+++ 3781 Maximum Score After Binary Swaps/3781maximum-score-after-binary-swaps.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long maximumScore(vector<int>& nums, string s) {
+        priority_queue<int>pq;
+        int n=nums.size();
+        long long ans=0;
+        for(int i=0;i<n;i++){
+            pq.push(nums[i]);
+            if(s[i]=='1'){
+                ans+=1ll*pq.top();
+                pq.pop();
+            }
+        }
+        return ans;
+    }
+};
+
