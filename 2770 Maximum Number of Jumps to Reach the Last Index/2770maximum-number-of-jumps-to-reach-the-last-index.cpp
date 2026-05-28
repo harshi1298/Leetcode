0c10bf44118f8c173753b47cb93afae486589e25
harshi1298 <11082004harshit@gmail.com>
2026-05-28 03:49:57 +0000

    Code challenge submission for question: 2770 Maximum Number of Jumps to Reach the Last Index

---
+++ 2770 Maximum Number of Jumps to Reach the Last Index/2770maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maximumJumps(vector<int>& nums, int target) {
+        int n=nums.size();
+        vector<int>dp(n,0);
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(abs(nums[i]-nums[j])<=target && (dp[j]!=0  || j==0)){
+                    dp[i]=max(dp[i],dp[j]+1);
+                }
+            }
+        }
+        return dp[n-1]==0?-1:dp[n-1];
+    }
+};
