533fe2824d1cefba3a70fca313e16cfea766ad5a
harshi1298 <11082004harshit@gmail.com>
2026-06-08 18:28:10 +0000

    Code challenge submission for question: 647 Palindromic Substrings

---
+++ 647 Palindromic Substrings/647palindromic-substrings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n=s.length();
+        vector<vector<bool>>dp(n,vector<bool>(n,0));
+        for(int len=1;len<=n;len++){
+            for(int i=0;i+len-1<n;i++){
+                int j=i+len-1;
+                if(len==1)dp[i][j]=true;
+                else if(len==2)dp[i][j]=(s[i]==s[j]);
+                else{
+                    dp[i][j]=((s[i]==s[j]) && dp[i+1][j-1]);
+                }
+            }
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(dp[i][j])ans++;
+            }
+        }
+        return ans;
+    }
+};
